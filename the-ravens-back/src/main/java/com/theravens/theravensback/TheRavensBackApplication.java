ba9166df81105aad72a493ce2f99dc0a72f4dfd1
Alexmz1 <menez.alexis19@gmail.com>
2023-08-04 09:54:55 +0200

    project initialization

---
+++ the-ravens-back/src/main/java/com/theravens/theravensback/TheRavensBackApplication.java
@@ -0,0 +1,13 @@
+package com.theravens.theravensback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRavensBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRavensBackApplication.class, args);
+	}
+
+}
